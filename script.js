2fb846a9a3e2dfc78f56f71819219aa6e26b8dc9
javicast55 <javieer55c@gmail.com>
2025-03-29 15:13:17 +0000

    finished

--- script.js
+++ script.js
@@ -16,12 +16,70 @@ function Onlynumbers(e) {
 }
 
 function Ret(num){
-    let previous = document.fo.values.value;
-    document.getElementById('value').value = previous + num;
+    let prevRet = document.fo.values.value;
+    document.getElementById('value').value = prevRet + num;
 }
 
 function Del(num){
-    let prev = document.fo.values.value;
-    let newChar = prev.substring(0,prev.length-1);
+    let prevDel = document.fo.values.value;
+    let newChar = prevDel.substring(0,prevDel.length-1);
     document.getElementById('value').value = newChar;
+}
+
+function Delall(){
+    document.getElementById('value').value = "";
+}
+
+function Check(num){
+    let prevCheck = document.fo.values.value;
+    if (prevCheck == ""){
+        document.fo.values.value="";
+    }
+    else{
+        prevCheck = document.fo.values.value;
+        document.getElementById('value').value = prevCheck + num;
+        let done = document.fo.values.value;
+
+        let record = 0;
+        let equal = 1;
+        let letterRecord;
+        let b=0;
+        let letter = "";
+
+        for (a = 1; a < done.length; a++){
+            if (done.charAt(a) == "+" || done.charAt(a) == "-" || done.charAt(a) == "*" || done.charAt(a) == "/" || done.charAt(a) == "."){
+                equal = equal + 1;
+                letter = done.charAt(a);
+            }
+            else{
+                if (equal > record){
+                    record = equal;
+                    letterRecord = letter;
+                }
+                equal = 1;
+            }
+            b = a;
+        }
+
+        if (equal > record){
+            record = equal;
+            letterRecord = letter;
+        }
+
+        if (record > 2){
+            let prev = done;
+            let newValue = prev.substring(0, prev.length-1);
+            document.getElementById('value').value = newValue;
+            record = 0;
+            b = 0;
+            equal = 1;
+            letter = "";
+            letterRecord = "";
+        }
+    }
+}
+
+function Calculate(){
+    let result = eval(document.fo.values.value);
+    document.fo.values.value = result;
 }
